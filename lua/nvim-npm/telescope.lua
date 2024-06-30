7168309c4f79b54ea79920ec558e1d05c0b227d5
sajjathossain <sajjathossain.official@gmail.com>
2024-07-01 02:05:27 +0600

    chore(telescope): cleanup

--- lua/nvim-npm/telescope.lua
+++ lua/nvim-npm/telescope.lua
@@ -1,8 +1,7 @@
 local M = {}
 local status_ok_telescope, telescope = pcall(require, 'telescope')
-local status_ok_fterm, fterm = pcall(require, 'FTerm')
 local status_ok_notify, notify = pcall(require, 'notify')
-if not status_ok_telescope or not status_ok_fterm or not status_ok_notify then return end
+if not status_ok_telescope or not status_ok_notify then return end
 telescope.setup {}
 
 local utils = require('nvim-npm.utils')
@@ -46,33 +45,19 @@ M._showScriptsInPackageJson = function(package_json_path)
     attach_mappings = function(prompt_bufnr, _)
       actions.select_default:replace(function()
         local selection = action_state.get_selected_entry()
-        print(vim.inspect(selection))
         actions.close(prompt_bufnr)
         local script_name = selection.value[1]
         local script_command = selection.value[2]
         local path = selection.value[3]
         if script_command then
           local dir = utils._fn.fnamemodify(path, ":h")
-          local cd = "cd " .. dir
           local script = utils._packageManagerCommand .. " " .. script_command
-          local cmd = cd .. " && " .. script
 
-          --[[ local ft = "fterm_" .. script_command
-          local term = fterm:new({
-            ft = ft,
-            cmd = cmd,
-            dimensions = {
-              height = 0.9,
-              width = 0.9
-            }
-          })
+          local name = " name=" .. dir:gsub('/', '-'):gsub("^%-", "") .. "::" .. script_command
+          local command = " cmd=" .. "\"" .. script .. "\""
+          local directory = " dir=" .. dir
 
-          term:open() ]]
-
-          fterm.run(cmd)
-
-          -- term:open()
-          -- fterm.scratch({ cmd = cmd })
+          vim.cmd("TermExec" .. name .. directory .. " size=25" .. " direction=float" .. command)
         else
           utils._api.nvim_err_writeln("No command found for script: " .. script_name)
         end
