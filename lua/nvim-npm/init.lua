7516b8701bf42bf2009e231d5d9ffab81c2d2e5c
sajjathossain <sajjathossain.official@gmail.com>
2024-07-01 17:47:27 +0600

    chore(init): expose some lua apis

--- lua/nvim-npm/init.lua
+++ lua/nvim-npm/init.lua
@@ -1,7 +1,13 @@
 local commands = require('nvim-npm.commands')
 local keymaps = require('nvim-npm.keymaps')
+local telescope = require('nvim-npm.telescope')
 
 local M = {}
+
+M.showScripts = telescope._showProjectsWithScriptsInTelescope
+M.openTerminal = telescope._openTerminal
+M.exitTerminal = telescope._exitTerminal
+M.exitAllTerminal = telescope._exitAllTerminals
 
 -- setup function
 --- @type function
